00757d2aa09bc1d32d630dde7a2e4181a56aebec
Phoenix <phoenixstroh@gmail.com>
2021-08-24 00:20:31 -0400

    Ignore DefaultCommonSkill

--- theSenseless/src/main/java/theSenseless/cards/DefaultCommonSkill.java
+++ theSenseless/src/main/java/theSenseless/cards/DefaultCommonSkill.java
@@ -1,5 +1,6 @@
 package theSenseless.cards;
 
+import basemod.AutoAdd;
 import com.megacrit.cardcrawl.actions.common.GainBlockAction;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
@@ -9,6 +10,7 @@ import theSenseless.characters.TheSenseless;
 
 import static theSenseless.SenselessMod.makeCardPath;
 
+@AutoAdd.Ignore
 public class DefaultCommonSkill extends AbstractDynamicCard {
 
     /*
