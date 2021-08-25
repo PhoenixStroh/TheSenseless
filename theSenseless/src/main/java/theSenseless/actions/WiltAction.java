2e857db5615573505b90707eaeff94572fe21997
Phoenix <phoenixstroh@gmail.com>
2021-08-25 01:32:18 -0400

    Semi Working Wilt Action

---
+++ theSenseless/src/main/java/theSenseless/actions/WiltAction.java
@@ -0,0 +1,83 @@
+package theSenseless.actions;
+
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.cards.CardGroup;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.AbstractCreature;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.core.Settings;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.localization.UIStrings;
+import com.megacrit.cardcrawl.actions.utility.NewQueueCardAction;
+
+public class WiltAction extends AbstractGameAction {
+    private static final UIStrings uiStrings = CardCrawlGame.languagePack.getUIString("DiscardPileToTopOfDeckAction");
+
+    public static final String[] TEXT = uiStrings.TEXT;
+
+    private AbstractPlayer p;
+    private CardGroup cardGroup;
+
+    public WiltAction(String cardGroup) {
+        this.p = AbstractDungeon.player;
+        switch (cardGroup)
+        {
+            case "draw": this.cardGroup = this.p.drawPile; break;
+            case "discard": this.cardGroup = this.p.discardPile; break;
+            case "exhaust": this.cardGroup = this.p.exhaustPile; break;
+        }
+        this.actionType = AbstractGameAction.ActionType.CARD_MANIPULATION;
+        this.duration = Settings.ACTION_DUR_FAST;
+    }
+
+    public WiltAction()
+    {
+        this("discard");
+    }
+
+    public void update() {
+        //If the battle is ending, end action.
+        if (AbstractDungeon.getCurrRoom().isBattleEnding()) {
+            this.isDone = true;
+            return;
+        }
+        // ???
+        if (this.duration == Settings.ACTION_DUR_FAST) {
+            //If the discard pile is empty, end action.
+            if (cardGroup.isEmpty()) {
+                this.isDone = true;
+                return;
+            } 
+            //If the discard pile is only 1, automatically play card.
+            if (cardGroup.size() == 1) {
+                AbstractCard tmp = cardGroup.getTopCard();
+                Wilt(tmp);
+            } 
+            //???
+            if (cardGroup.group.size() > this.amount) {
+                AbstractDungeon.gridSelectScreen.open(cardGroup, 1, TEXT[0], false, false, false, false);
+                tickDuration();
+                return;
+            } 
+        } 
+        //If the cards are selected, do action.
+        if (!AbstractDungeon.gridSelectScreen.selectedCards.isEmpty()) {
+            for (AbstractCard c : AbstractDungeon.gridSelectScreen.selectedCards) {
+                Wilt(c);
+            } 
+            AbstractDungeon.gridSelectScreen.selectedCards.clear();
+            AbstractDungeon.player.hand.refreshHandLayout();
+        } 
+        tickDuration();
+    }
+
+    public void Wilt(AbstractCard c)
+    {
+        c.exhaust = true;
+        cardGroup.group.remove(c);
+        (AbstractDungeon.getCurrRoom()).souls.remove(c);
+        addToBot((AbstractGameAction)new NewQueueCardAction(c, true, false, true));
+    }
+}
+
---
+++ theSenseless/src/main/java/theSenseless/cards/CardForgottenTechnique.java
@@ -0,0 +1,83 @@
+package theSenseless.cards;
+
+import com.megacrit.cardcrawl.actions.utility.ScryAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import theSenseless.SenselessMod;
+import theSenseless.actions.UpToDiscardAction;
+import theSenseless.characters.TheSenseless;
+import theSenseless.actions.WiltAction;
+
+import static theSenseless.SenselessMod.makeCardPath;
+
+// public class ${NAME} extends AbstractDynamicCard
+
+public class CardForgottenTechnique extends AbstractDynamicCard {
+
+    /*
+     * "Hey, I wanna make a bunch of cards now." - You, probably.
+     * ok cool my dude no problem here's the most convenient way to do it:
+     *
+     * Copy all of the code here (Ctrl+A > Ctrl+C)
+     * Ctrl+Shift+A and search up "file and code template"
+     * Press the + button at the top and name your template whatever it is for - "AttackCard" or "PowerCard" or something up to you.
+     * Read up on the instructions at the bottom. Basically replace anywhere you'd put your cards name with ${NAME}
+     * And then you can do custom ones like ${DAMAGE} and ${TARGET} if you want.
+     * I'll leave some comments on things you might consider replacing with what.
+     *
+     * Of course, delete all the comments and add anything you want (For example, if you're making a skill card template you'll
+     * likely want to replace that new DamageAction with a gain Block one, and add baseBlock instead, or maybe you want a
+     * universal template where you delete everything unnecessary - up to you)
+     *
+     * You can create templates for anything you ever want to. Cards, relics, powers, orbs, etc. etc. etc.
+     */
+
+    // TEXT DECLARATION
+
+    // public static final String ID = DefaultMod.makeID(${NAME}.class.getSimpleName()); // USE THIS ONE FOR THE TEMPLATE;
+    public static final String ID = SenselessMod.makeID("CardForgottenTechnique"); // DELETE THIS ONE.
+    public static final String IMG = makeCardPath("Attack.png");// "public static final String IMG = makeCardPath("${NAME}.png");
+    // This does mean that you will need to have an image with the same NAME as the card in your image folder for it to run correctly.
+
+
+    // /TEXT DECLARATION/
+
+
+    // STAT DECLARATION
+
+    private static final CardRarity RARITY = CardRarity.COMMON; //  Up to you, I like auto-complete on these
+    private static final CardTarget TARGET = CardTarget.SELF;  //   since they don't change much.
+    private static final CardType TYPE = CardType.SKILL;       //
+    public static final CardColor COLOR = TheSenseless.Enums.COLOR_GRAY;
+
+    private static final int COST = 1;  // COST = ${COST}
+
+    // /STAT DECLARATION/
+
+
+    public CardForgottenTechnique() { // public ${NAME}() - This one and the one right under the imports are the most important ones, don't forget them
+        super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
+
+    }
+
+
+    // Actions the card should do.
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        AbstractDungeon.actionManager.addToBottom(new WiltAction());
+    }
+
+
+    // Upgraded stats.
+    @Override
+    public void upgrade() {
+        if (!upgraded) {
+            upgradeName();
+
+            
+
+            initializeDescription();
+        }
+    }
+}
