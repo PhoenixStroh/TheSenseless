c8cb7aca02ebb66f1dc94bd2cd7881ca1dbde2cf
Phoenix <phoenixstroh@gmail.com>
2021-09-04 12:01:22 -0400

    Cleanup BattleCry

--- theSenseless/src/main/java/theSenseless/cards/CardBattleCry.java
+++ theSenseless/src/main/java/theSenseless/cards/CardBattleCry.java
@@ -4,7 +4,6 @@ import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
-import com.megacrit.cardcrawl.powers.VulnerablePower;
 import com.megacrit.cardcrawl.powers.WeakPower;
 
 import theSenseless.SenselessMod;
